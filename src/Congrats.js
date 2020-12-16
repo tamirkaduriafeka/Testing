37069b994a1a0ab9a79511910de0dbe1d62ca0f1
kadtamir <kadtamir@gmail.com>
2020-12-16 12:29:48 +0200

    Initial Commit

---
+++ src/Congrats.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Congrats(props) {
+    if (props.success){
+      return (
+        <div data-test="component-congrats">
+          <span data-test="congrats-message">
+            Congratulations! You guessed the word!
+          </span>
+        </div>
+      )
+    } else {
+        return (
+          <div data-test="component-congrats">
+          </div>
+          )
+    }
+}
+
+Congrats.propTypes ={
+  success: propTypes.bool.isRequired
+};
+
+export default Congrats
---
+++ test/testUtils.js
@@ -0,0 +1,10 @@
+import { checkPropTypes } from 'prop-types';
+
+export const findByTestAttr = (wrapper,val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
+export const checkProp = (component, conformingProps) =>{
+  const propError = checkPropTypes(component.propTypes,conformingProps,'prop',component.name);
+  expect(propError).toBeUndefined();
+}
